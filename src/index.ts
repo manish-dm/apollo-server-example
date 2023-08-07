9178c94ad5d5738f1fccf60e8da3a7fbb9ed7944
manish <themk85@gmail.com>
2023-08-07 09:03:52 +0530

    initial graphql setup

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
+
+import dotenv from "dotenv";
+dotenv.config();
+// import config from "config";
+
+const app = express();
+
+const httpServer = http.createServer(app);
+
+const typeDefs = `#graphql
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+await server.start();
+
+app.use(
+  "/graphql",
+  cors<cors.CorsRequest>(),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  })
+);
+
+await new Promise<void>((resolve) =>
+  httpServer.listen({ port: 4001 }, resolve)
+);
+
+console.log(`🚀 Server ready at http://localhost:4001/graphql`);
